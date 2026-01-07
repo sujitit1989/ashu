9434d6772d4d1ff004025678a36bde60f27ca599
sujit <sujitit1989@gmail.comm>
2026-01-07 10:46:00 +0530

    oher rg add

--- tudu_infra/main.tf
+++ tudu_infra/main.tf
@@ -3,6 +3,11 @@ module "resource_group" {
   resource_group_name = "rg_toduapp"
   resource_group_location = "centralindia"
 }
+module "resource_group" {
+  source = "../modules/azure_resource_group"
+  resource_group_name = "rg_toduapp1"
+  resource_group_location = "centralindia"
+}
 module "virtual_network" {
     depends_on = [ module.resource_group ]
     source = "../modules/azure_virtual_network"
